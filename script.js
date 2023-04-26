ad98b368def9c1d2028952c8de0b7c3f1df6e579
techmoves <muhndlovu28@gmail.com>
2023-04-26 22:07:57 +0200

    updated both mobile and destop

--- script.js
+++ script.js
@@ -1,57 +1,69 @@
-const menuBtn = document.querySelector(".humburger");
+// const menuBtn = document.querySelector(".humburger");
 
-const menuDisplay = () => {
-  const modal = document.createElement("section");
-  modal.id = "custom-modal";
+// const menuDisplay = () => {
+//   const modal = document.createElement("section");
+//   modal.id = "custom-modal";
 
-  const deleteBtn = document.createElement("div");
-  deleteBtn.textContent = "X";
-  deleteBtn.id = "modal-delete";
-  modal.appendChild(deleteBtn);
+//   const deleteBtn = document.createElement("div");
+//   deleteBtn.textContent = "X";
+//   deleteBtn.id = "modal-delete";
+//   modal.appendChild(deleteBtn);
 
-  const menuList = document.createElement("ul");
-  menuList.id = "menu-list";
+//   const menuList = document.createElement("ul");
+//   menuList.id = "menu-list";
 
-  const item1 = document.createElement("li");
-  const portfolio = document.createElement("a");
-  portfolio.href = "#work-section";
-  portfolio.className = "menu-content";
-  portfolio.textContent = "Portfolio";
-  item1.appendChild(portfolio);
-  portfolio.addEventListener("click", () => {
-    modal.style.visibility = "hidden";
-  });
+//   const item1 = document.createElement("li");
+//   const portfolio = document.createElement("a");
+//   portfolio.href = "#about-sec";
+//   portfolio.className = "menu-content";
+//   portfolio.textContent = "About";
+//   item1.appendChild(About);
+//   portfolio.addEventListener("click", () => {
+//     modal.style.visibility = "hidden";
+//   });
 
-  const item2 = document.createElement("li");
-  const about = document.createElement("a");
-  about.href = "#about-me-sec";
-  about.className = "menu-content";
-  about.textContent = "About";
-  item2.appendChild(about);
-  about.addEventListener("click", () => {
-    modal.style.visibility = "hidden";
-  });
+//   const item2 = document.createElement("li");
+//   const about = document.createElement("a");
+//   about.href = "#about-sec";
+//   about.className = "menu-content";
+//   about.textContent = "About";
+//   item2.appendChild(about);
+//   about.addEventListener("click", () => {
+//     modal.style.visibility = "hidden";
+//   });
 
-  const item3 = document.createElement("li");
-  const contact = document.createElement("a");
-  contact.href = "#form-area1";
-  contact.className = "menu-content";
-  contact.textContent = "Contact";
-  item3.appendChild(contact);
-  contact.addEventListener("click", () => {
-    modal.style.visibility = "hidden";
-  });
+//   menuList.appendChild(item1);
+//   menuList.appendChild(item2);
+//   menuList.appendChild(item3);
+//   modal.appendChild(menuList);
+//   document.body.appendChild(modal);
 
-  menuList.appendChild(item1);
-  menuList.appendChild(item2);
-  menuList.appendChild(item3);
-  modal.appendChild(menuList);
-  document.body.appendChild(modal);
+//   const removeModal = () => {
+//     modal.style.visibility = "hidden";
+//   };
 
-  const removeModal = () => {
-    modal.style.visibility = "hidden";
-  };
+//   deleteBtn.addEventListener("click", removeModal);
+// };
+// menuBtn.addEventListener("click", menuDisplay);
 
-  deleteBtn.addEventListener("click", removeModal);
-};
-menuBtn.addEventListener("click", menuDisplay);
+
+// Mobile menu
+
+const ham = document.querySelector(".humburger");
+const closeBtn = document.querySelector('.closeBtn');
+const mobMenu = document.querySelector('.mob-menu');
+
+ham.addEventListener('click', () => {
+  mobMenu.style.display = 'flex';
+  mainContainer.style.filter = 'blur(100px)';
+});
+
+closeBtn.addEventListener('click', () => {
+  mobMenu.style.display = 'none';
+  mainContainer.style.filter = 'blur(0)';
+});
+
+mobMenu.addEventListener('click', () => {
+  mobMenu.style.display = 'none';
+  mainContainer.style.filter = 'blur(0)';
+});
