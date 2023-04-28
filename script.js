439ad95f0b751bff8c6aa1974d27adfd57cb4180
techmoves <muhndlovu28@gmail.com>
2023-04-28 21:48:13 +0200

    Modified desto version and mobile

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-
 // mobile Menu
 
-const hum = document.querySelector(".humburger");
-const closeBtn = document.querySelector("#closebtn");
+const hum = document.querySelector('.humburger');
+const closeBtn = document.querySelector('#closebtn');
 
-hum.addEventListener("click", (e) => {
-  const header = document.querySelector("header");
-  const mobMenu = document.querySelector(".option");
-  header.style.display = "none";
-  mobMenu.classList.toggle("show");
+hum.addEventListener('click', (e) => {
+  e.preventDefault();
+  const header = document.querySelector('header');
+  const mobMenu = document.querySelector('.option');
+  header.style.display = 'none';
+  mobMenu.classList.toggle('show');
 });
 
-closeBtn.addEventListener("click", (e) => {
-  const header = document.querySelector("header");
-  const mobMenu = document.querySelector(".option");
-  header.style.display = "block";
-  mobMenu.classList.toggle("show");
+closeBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const header = document.querySelector('header');
+  const mobMenu = document.querySelector('.option');
+  header.style.display = 'block';
+  mobMenu.classList.toggle('show');
 });
